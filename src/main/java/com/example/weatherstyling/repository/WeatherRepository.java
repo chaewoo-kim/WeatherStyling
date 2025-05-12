8f04fa46efd86a8cfb2786d9519b03db728b25d6
chaewoo-kim <chaewookim2877@gmail.com>
2025-05-12 22:56:18 +0900

    stringParser 오류 해결

---
+++ src/main/java/com/example/weatherstyling/repository/WeatherRepository.java
@@ -0,0 +1,16 @@
+package com.example.weatherstyling.repository;
+
+import com.example.weatherstyling.model.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface WeatherRepository extends JpaRepository<Weather, Long> {
+
+    boolean existsByUrl(String url);
+
+    Optional<Weather> findByUrl(String url);
+
+
+}
